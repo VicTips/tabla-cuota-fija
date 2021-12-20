9825afe781231c1b450394984e711ca23ac2c752
victor_arias <torvicarias@gmail.com>
2021-12-20 17:32:50 -0500

    Se añaden Text fields y margen superior de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css';
+import "./App.css";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container className="Container">
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          required
+          variant="filled"
+          label="Valor del crédito"
+          type="number"
+          color="success"
+        />
+        <TextField
+          required
+          variant="filled"
+          label="Número de periodos"
+          type="number"
+          color="success"
+        />
+        <TextField
+          required
+          variant="filled"
+          label="Tasa de interés"
+          type="number"
+          color="success"
+        />
+      </Box>
+    </Container>
   );
 }
 
