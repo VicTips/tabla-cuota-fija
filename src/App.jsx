a763ad9db2fedc2fc9eec8aa8754ee9136cfaa59
victips <torvicarias@gmail.com>
2021-12-27 15:44:14 -0600

    Se incorpora el componente Box en App.jsx para mejorar el layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Inputs from "./components/Input";
 import { useState } from "react";
 import AmortizationTable from "./components/AmortizationTable";
+import Box from "@mui/material/Box";
 
 function App() {
   const [loan, setLoan] = useState("");
   const [nper, setNper] = useState("");
   const [rate, setRate] = useState("");
   return (
-    <>
+    <Box m={{xs: 2, sm: 3, md: 4, lg: 5}}>
       <Inputs
         onChange={(loan, nper, rate) => {
           setLoan(loan);
@@ -17,7 +18,7 @@ function App() {
         }}
       />
       <AmortizationTable loan={loan} nper={nper} rate={rate} />
-    </>
+    </Box>
   );
 }
 
